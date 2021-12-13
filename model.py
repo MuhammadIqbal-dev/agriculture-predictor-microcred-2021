b38730910d2f75792b9c694143e66dfe1f8506eb
MuhammadIqbal-hehe <73480353+MuhammadIqbal-dev@users.noreply.github.com>
2021-12-13 13:34:18 +0700

    Add files via upload

--- model.py
+++ model.py
@@ -1,11 +1,4 @@
-import pandas as pd
-import numpy as np
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from sklearn.model_selection import train_test_split
-from sklearn import linear_model
 
 coef = -51.36637103
 intercept = 111115.3745066
